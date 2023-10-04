2b9a8eb79b5f4d9ec095abb97cbf010fe043392c
rohitei <rohit.eminenceinnovation@gmail.com>
2023-10-04 12:19:37 +0530

    updated

--- src/main/java/com/apigateway/controller/FallBackController.java
+++ src/main/java/com/apigateway/controller/FallBackController.java
@@ -3,6 +3,7 @@ package com.apigateway.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apigateway.entity.ResponseBean;
@@ -10,22 +11,22 @@ import com.apigateway.entity.ResponseBean;
 @RestController
 public class FallBackController {
 	
-	@GetMapping("/userServiceFallBack")
+	@RequestMapping("/userServiceFallBack")
 	public ResponseEntity<?> userServiceFallback(){
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseBean("UserService is down"));
 	}
 	
-	@GetMapping("/encryptionServiceFallBack")
+	@RequestMapping("/encryptionServiceFallBack")
 	public ResponseEntity<?> encryptionServiceFallback(){
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseBean("EncryptDecryptService is down"));
 	}
 	
-	@GetMapping("/userPanelFallBack")
+	@RequestMapping("/userPanelFallBack")
 	public ResponseEntity<?> userPanelFallback(){
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseBean("UserPanelFrontend is down"));
 	}
 	
-	@GetMapping("/adminPanelFallBack")
+	@RequestMapping("/adminPanelFallBack")
 	public ResponseEntity<?> frontEndPanelFallback(){
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseBean("AdminPanelFrontend is down"));
 	}
